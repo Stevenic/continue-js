3ecc1e1608a7de3d52a8cb5f1a776b666f67b5ea
Steven Ickman <stevenic@microsoft.com>
2022-05-08 16:16:32 -0700

    Moved registrations to bottom of file

--- samples/02-EchoBot/continuations.js
+++ samples/02-EchoBot/continuations.js
@@ -1,6 +1,5 @@
 const { canContinueWith, continueWith, dontContinue } = require('@stevenic/continue-js');
 
-canContinueWith(startContinuation);
 async function startContinuation(context) {
     if (!context.conversationState.name) {
         // Prompt for users name and chain response to namePromptContinuation
@@ -13,13 +12,16 @@ async function startContinuation(context) {
     }
 }
 
-canContinueWith(namePromptContinuation);
 async function namePromptContinuation(context) {
     // Save users response and end conversation
     context.conversationState.name = context.activity.text;
     await context.sendActivity(`Nice to meet you ${context.conversationState.name}! You can say stuff to me and I'll echo it back.`);
     return await dontContinue();
 }
+
+// Register continuations.
+canContinueWith(startContinuation);
+canContinueWith(namePromptContinuation);
 
 // We only need to export the root continuation
 module.exports.startContinuation = startContinuation;
