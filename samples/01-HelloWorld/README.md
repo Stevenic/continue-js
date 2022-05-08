91319542aebba3a2e48c3f5964fd4f57f68f9f93
Steven Ickman <stevenic@microsoft.com>
2022-05-08 11:16:54 -0700

    Added lerna and a sample

---
+++ samples/01-HelloWorld/README.md
@@ -0,0 +1,11 @@
+# 01 - Hello World
+
+A simple JavaScript sample app that calls a test continuation 5 times.
+
+## Usage
+
+To run the sample first follow the [build instructions](../../README.md#building) for the project. Then from a command shell type:
+
+```Bash
+$ node app.js
+``` 
---
+++ samples/01-HelloWorld/app.js
@@ -0,0 +1,34 @@
+const { canContinueWith, continueWith, continueNow, dontContinue } = require('continue-js');
+
+// Define test continuation
+async function testContinuation(context) {
+    // Print call number and increment iteration count
+    console.log(`This is call ${++context.iterations} of ${context.maxIterations}`);
+    
+    // Chain back to self until max iterations met
+    if (context.iterations < context.maxIterations) {
+        return await continueWith(testContinuation);
+    } else {
+        return await dontContinue();
+    }
+}
+
+// Register continuations with Continue JS
+canContinueWith(testContinuation);
+
+// Execute continuation 5 times
+(async () => {
+    // Define a context object that will be passed to the continuation
+    const context = { iterations: 0, maxIterations: 5 };
+
+    // Create initial continuation using continueWith()
+    let continuation = await continueWith(testContinuation);
+
+    // Loop over continuations until told we're done
+    // - continueNow() executes the current continuation and returns the next one
+    while (!continuation.done) {
+        continuation = await continueNow(continuation, context);
+    }
+})();
+
+
